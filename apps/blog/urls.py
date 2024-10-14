2e0ffa8a76fd0503561554290aae37a0e005ee4f
Roberto Galeano <robx.galeano@gmail.com>
2024-10-14 14:18:34 -0300

    Creada la plantilla registro

--- apps/blog/urls.py
+++ apps/blog/urls.py
@@ -23,4 +23,5 @@ urlpatterns = [
     path('about', views.about_us, name='about-us'),
     path('contactanos/', views.contactanos, name='contactanos'),
     path('login', views.login, name='login'),
+    path('register', views.register, name='register')
 ]
