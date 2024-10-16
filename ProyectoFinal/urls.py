8eabd39ae9780840e1fae30f9a9b1dd95d439b74
ileFontana <ileanafontana@live.com>
2024-10-16 02:41:36 -0300

    cambios en la carpeta blog_auth

--- ProyectoFinal/urls.py
+++ ProyectoFinal/urls.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from django.urls import path, include  # Include 'include' if not already imported
+# Include 'include' if not already imported
+from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 
@@ -9,4 +10,5 @@ urlpatterns = [
 ]
 
 if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    urlpatterns += static(settings.MEDIA_URL,
+                          document_root=settings.MEDIA_ROOT)
--- apps/blog_auth/views.py
+++ apps/blog_auth/views.py
@@ -1,3 +1,22 @@
 from django.shortcuts import render
-
+from django.views.generic import FormView
+from .forms import SignUpForm
+from django.urls import reverse_lazy
+from django.contrib.auth import views as auth_views
 # Create your views here.
+# CBV
+
+
+class SignUpView(FormView):
+    template_name = "registration/register.html"
+    form_class = SignUpForm
+    success_url = reverse_lazy("apps.blog_auth:login")
+
+    def def form_valid(self, form):
+        form.save()
+        return super().form_valid(form)
+
+
+class LoginView(auth_views.LoginView):
+    template_name = "registration/login.html"
+    next_page = "home"
