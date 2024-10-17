8593349a6623616bcdc6bcd44e7ebfed15613e08
AguirreNicolas 12 <nicolas1aguirre2@gmail.com>
2024-10-17 17:20:52 -0300

    Solucion o intento de solucion

--- apps/blog/urls.py
+++ apps/blog/urls.py
@@ -22,8 +22,7 @@ urlpatterns = [
     path('', views.home, name='blog-home'),
     path('about/', views.about_us, name='about-us'),
     path('like/<int:post_id>/', views.Like_post, name='like_post'),
-    path('like/<int:comentario_id>/',
-         views.Like_comentario, name='like_comentario'),
+    path('like/<int:comentario_id>/',views.Like_comentario, name='like_comentario'),
 
     path('blog/', views.blog, name='blog'),
     path('blog/<str:url>/', views.noticia, name='noticia'),
