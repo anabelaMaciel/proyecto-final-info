0bf6e7f8e9f42e1d9c637681fa52db92ebdaf50d
ileFontana <ileanafontana@live.com>
2024-10-17 02:35:40 -0300

    cambio y CRUD en blog_auth

--- apps/blog_auth/models.py
+++ apps/blog_auth/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Eventos(models.Model):
+    titulo = models.CharField(max_length=200)
+    descripcion = models.TextField()
+    fecha = models.DateTimeField()
+    ubicación = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.titulo
