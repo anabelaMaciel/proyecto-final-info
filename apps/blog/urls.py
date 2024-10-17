c5709fb75328b513b8440dcf587092e95bc59d7e
AguirreNicolas 12 <nicolas1aguirre2@gmail.com>
2024-10-17 14:52:27 -0300

    solucion de cambio -  modified:   apps/blog/urls.py

--- apps/blog/urls.py
+++ apps/blog/urls.py
@@ -29,5 +29,6 @@ urlpatterns = [
     path('login/', views.login, name='login'),
     path('register/', views.register, name='register'),
     path('categorias/', views.categorias, name='categorias'),
+ 
 
 ]
