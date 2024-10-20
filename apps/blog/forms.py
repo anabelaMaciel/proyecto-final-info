d8d8e0ffa740fe54734e4713444d1b9c153b4d42
Ina <ina@gmail.com>
2024-10-20 00:32:33 -0300

    Registro, iniciar sesion, colaboradores. (Inna desde otra pc)

--- apps/blog/forms.py
+++ apps/blog/forms.py
@@ -3,17 +3,23 @@ from django.forms import ModelForm
 from django import forms
 from .models import Comentarios, Categorias, Posts
 
+from .models import Usuario_personalizado
+from django.contrib.auth.forms import UserCreationForm
+
 # En nuestra clase Meta definimos el modelo, con el cual va a interactuar nuestro formulario
 # y los campos que vamos a permitir que completen los usuarios
-
-
-# Formulario de Categorias
 
 class CategoriasForm(forms.ModelForm):
     class Meta:
         model = Categorias
         fields = ['nombre', 'imagen']
 
+class UserRegisterForm(UserCreationForm):
+    email = forms.EmailField()
+
+    class Meta:
+        model = Usuario_personalizado
+        fields = ['username', 'email', 'password1', 'password2']
 
 def listar_categorias(request):
     categorias = []
