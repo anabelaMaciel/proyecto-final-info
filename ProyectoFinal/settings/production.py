0916f292b6383b809d971080e46341c00c48d9aa
Ana Maciel <anabelacelestemaciel@gmail.com>
2024-10-20 23:15:58 -0300

    Configuracion de deploy

--- ProyectoFinal/settings/production.py
+++ ProyectoFinal/settings/production.py
@@ -4,7 +4,7 @@ import os
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['Technofilos.pythonanywhere.com']
 
 DATABASES = {
     'default': {
